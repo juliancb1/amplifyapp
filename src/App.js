022f93b233ae0ab250c4f9ec45c08cb32c621119
juliancb1 <73367261+juliancb1@users.noreply.github.com>
2020-10-24 12:18:53 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="www.theage.com.au"
           target="_blank"
           rel="noopener noreferrer"
         >
